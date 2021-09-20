638e32fb1667c0278b2d7c99322b85998a18a1f0
Ty Baldwin <ty@torqdl.com>
2021-09-20 08:05:54 -0600

    Working on online testing for demo mode

--- PyDDNS.py
+++ PyDDNS.py
@@ -1,11 +1,58 @@
 #!/usr/bin/env python3
 
-""" 
+"""
     This is a python script for updating DNS records in Dreamhost or Cloudflare using their
     respective APIs.
 
     Provided under the MIT License (MIT). See LICENSE for details.
 """
+
+"""
+    Imports
+"""
+import logging
+import time
+import sys
+import syslog
+import os
+import json
+import urllib
+import socket
+import platform
+from urllib.request import urlopen
+# import requests
+# import urllib.request as urlr
+# import uuid
+# import signal
+# import threading
+# import re
+# import pandas as pd
+
+"""
+    Logging Level
+"""
+# Set logging level
+logging.basicConfig(level=logging.DEBUG)
+
+# Print the time for informational and debugging purposes
+print("Current Time: ", time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
+
+"""
+    Python Version Check
+"""
+if not (sys.version_info.major == 3 and sys.version_info.minor >= 5):
+    print("PyDDNS requires Python 3.5 or higher!")
+    print("You are using Python {}.{}.".format(sys.version_info.major, sys.version_info.minor))
+    msg = 'PyDDNS requires Python 3.5 or higher! You are using Python {}.{}.'.format(sys.version_info.major, sys.version_info.minor)
+    syslog.syslog(syslog.LOG_ERR, msg)
+    sys.exit(msg)
+
+"""
+    Change to directory where script is
+"""
+# Changing directory to where the script is running from. Uncomment next print line for debug
+# print("chdir to", sys.path[0])
+os.chdir(sys.path[0])
 
 """
     DEMO_MODE
@@ -15,33 +62,9 @@
 """
 DEMO_MODE = True
 
-# Set logging level
-import logging
-logging.basicConfig(level=logging.DEBUG)
-
-#Python version check
-import sys
-import syslog
-if sys.version_info.major < 3:
-    msg = 'This script requires Python 3. It cannot run. Please install Python 3 and try again.'
-    syslog.syslog(syslog.LOG_ERR, msg)
-    sys.exit(msg)
-
-import urllib.request as urlr
-import uuid
-import json
-# import requests
-import json
-import sys
-import signal
-import os
-import time
-import threading
-import re
-import socket
-import platform
-# import pandas as pd
-
+"""
+    IP Address Determinators
+"""
 def checkValidIP(ip, type):
     try:
         if type == "ipv4":
@@ -53,13 +76,11 @@ def checkValidIP(ip, type):
         return True
     except socket.error:
         return False
-
 def getIPs(use_local_ip, ipv4_enabled, ipv6_enabled):
     if use_local_ip:
         return getLocalIPs(ipv4_enabled, ipv6_enabled)
     else:
         return getPublicIPs(ipv4_enabled, ipv6_enabled)
-
 def getLocalIPs(ipv4_enabled, ipv6_enabled):
     # get local IPs
     if platform.system() == "Linux":
@@ -74,7 +95,6 @@ def getLocalIPs(ipv4_enabled, ipv6_enabled):
             ip_list.append(net[-1][0])
     else:
         raise ValueError("os type error, check result of platform.system()")
-
     ips = {}
     forbid_prefix = ["127.", "172.", "192.", "fe80:"]
     for ip in ip_list:
@@ -91,7 +111,6 @@ def getLocalIPs(ipv4_enabled, ipv6_enabled):
                 "ip": ip
             }
     return ips
-
 def getPublicIPs(ipv4_enabled, ipv6_enabled):
     # get public IPs
     a = None
@@ -124,96 +143,123 @@ def getPublicIPs(ipv4_enabled, ipv6_enabled):
         }
     return ips
 
+"""
+    Authentication
+"""
 def get_authentication(DNS_Provider):
     AUTH = []
     PROVIDER = DNS_Provider['provider']
+
     if PROVIDER == 'cloudflare':
-        logging.debug("Getting Authentication for Cloudflare...")
         if 'api_key' in DNS_Provider['authentication'] and DNS_Provider['authentication']['api_key']:
             AUTH.append(DNS_Provider['authentication']['api_key'])
         else:
-            logging.error("Cloudflare API key is missing or blank...")
             msg = 'The Cloudflare API key is missing or blank. Please edit your PyDDNS configuration and try again.'
             syslog.syslog(syslog.LOG_ERR, msg)
             sys.exit(msg)
         if 'account_email' in DNS_Provider['authentication'] and DNS_Provider['authentication']['account_email']:
             AUTH.append(DNS_Provider['authentication']['account_email'])
         else:
-            logging.error("Cloudflare account email is missing or blank...")
             msg = 'The Cloudflare account email is missing or blank. Please edit your PyDDNS configuration and try again.'
             syslog.syslog(syslog.LOG_ERR, msg)
             sys.exit(msg)
         if 'api_token' in DNS_Provider['authentication'] and DNS_Provider['authentication']['api_token']:
             AUTH.append(DNS_Provider['authentication']['api_token'])
         else:
-            logging.error("Cloudflare API token is missing or blank...")
             msg = 'The Cloudflare API token is missing or blank. Please edit your PyDDNS configuration and try again.'
             syslog.syslog(syslog.LOG_ERR, msg)
             sys.exit(msg)
+
     elif PROVIDER == 'dreamhost':
-        logging.debug("Getting Authentication for Dreamhost...")
-        if 'api_key' in DNS_Provider['authentication']:
+        if 'api_key' in DNS_Provider['authentication'] and DNS_Provider['authentication']['api_key']:
             AUTH.append(DNS_Provider['authentication']['api_key'])
         else:
-            logging.error("Dreamhost API key is missing or blank...")
             msg = 'The Dreamhost API key is missing or blank. Please edit your PyDDNS configuration and try again.'
             syslog.syslog(syslog.LOG_ERR, msg)
             sys.exit(msg)
+
     else:
-        logging.error("Cannot get authentication parameters. DNS provider not recognized...")
-        msg = 'The DNS provider cannot be recognized. Please edit your PyDDNS configuration and try again.'
+        msg = 'The DNS provider cannot be recognized. Only Dreamhost and Cloudflare are currently supported. Please edit your PyDDNS configuration and try again.'
         syslog.syslog(syslog.LOG_ERR, msg)
         sys.exit(msg)
-    
     return AUTH
 
-def update_dns_cloudflare():
-    logging.debug("Updating Cloudflare DNS...")
+"""
+    Cloudflare Updater
+"""
+def update_dns_cloudflare(AUTH, ZONEID, RECORD):
+    print("\nCloudflare Info:\nAuth: ", AUTH, "\nZone: ", ZONEID, "\nRecord: ", RECORD)
 
-def update_dns_dreamhost():
-    logging.debug("Updating Dreamhost DNS...")
+"""
+    Dreamhost Updater
+"""
+def update_dns_dreamhost(AUTH, RECORD):
+    print("\nDreamhost Info:\nAuth: ", AUTH, "\nRecord: ", RECORD)
 
-def make_it_so():
-
-    print(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
-    # print("chdir to", sys.path[0])
-    os.chdir(sys.path[0])
-    # check python version
-    version = float(str(sys.version_info[0]) + "." + str(sys.version_info[1]))
-    if(version < 3.5):
-        raise Exception("This script requires Python 3.5+")
+"""
+    Process DNS Provider Info
+"""
+def process_providers_from_json(CONFIG_DATA):
+    # read each DNS provider entry
+    for PROVIDER_INDEX, DNS_PROVIDER in enumerate(CONFIG_DATA['dns']):
+        PROVIDER = DNS_PROVIDER['provider']
         
-    # this finds our json config files
-    PATH_TO_CONFIG_FILES = os.getcwd() + "/config/"
-    if DEMO_MODE:
-        logging.info("Running in Demo mode, so only finding sample config file, such as sample_config.json and sample_config2.json")
-        CONFIG_FILES = [FILE_NAME for FILE_NAME in os.listdir(PATH_TO_CONFIG_FILES) if FILE_NAME.startswith('sample_config') and FILE_NAME.endswith('.json')]
-    else:
-        CONFIG_FILES = [FILE_NAME for FILE_NAME in os.listdir(PATH_TO_CONFIG_FILES) if FILE_NAME.endswith('.json')]
+        AUTH = get_authentication(DNS_PROVIDER)
+        # return should contain [API_Key, Account_Email, API_Token]
 
+        # check which provider and process the records accordingly
+        if PROVIDER == 'cloudflare':
+            for ZONE_INDEX, ZONE in enumerate(DNS_PROVIDER['zones']):
+                ZONEID = ZONE['zone_id']
+
+                for RECORD_INDEX, RECORD in enumerate(ZONE['records']):
+                    update_dns_cloudflare(AUTH, ZONEID, RECORD)
+
+        elif PROVIDER == 'dreamhost':
+            for RECORD_INDEX, RECORD in enumerate(DNS_PROVIDER['records']):
+                update_dns_dreamhost(AUTH, RECORD)
+
+"""
+    Process local .json configuration files
+"""
+def process_local_json(PATH_TO_CONFIG_FILES, CONFIG_FILES):
     # we need both the json and an index number so use enumerate()
     for JSON_FILE_INDEX, CONFIG_FILE in enumerate(CONFIG_FILES):
         with open(os.path.join(PATH_TO_CONFIG_FILES, CONFIG_FILE)) as JSON_FILE:
             CONFIG_DATA = json.load(JSON_FILE)
-            logging.debug("\n\nConfig data:\n%s", CONFIG_DATA)
+            process_providers_from_json(CONFIG_DATA)
 
-            # read each DNS provider entry
-            for Provider_Index, DNS_Provider in enumerate(CONFIG_DATA['dns']):
-                logging.debug("\n\n%s:%s", Provider_Index, DNS_Provider['provider'])
-                PROVIDER = DNS_Provider['provider']
-                
-                AUTH = get_authentication(DNS_Provider)
-                logging.debug("Authentication: %s", AUTH)
-                # return should contain [API_Key, Account_Email, API_Token]
+def process_online_json():
+    if DEMO_MODE:
+        print("\nPyDDNS cannot find sample config files. It will use the sample from the PyDDNS GitHub repository.")
+        url = "https://raw.githubusercontent.com/TORQDL/pyddns/initial_build/config/sample_config.json"
+        CONFIG_DATA = json.loads(urlopen(url).read().decode("UTF-8"))
+        process_providers_from_json(CONFIG_DATA)
 
-                # check which provider and process accordingly
-                if PROVIDER == 'cloudflare':
-                    # TODO itterate over zone info
-                    # TODO itterate over records in each zone
-                    update_dns_cloudflare(AUTH, ZONE, RECORD)
-                elif PROVIDER == 'dreamhost':
-                    # TODO itterate over records
-                    update_dns_dreamhost(AUTH, RECORD)
+"""
+    Start Script
+"""
+def make_it_so():
+    PATH_TO_CONFIG_FILES = os.getcwd() + "/config/"
+    if DEMO_MODE:
+        print("\nPyDDNS is running in Demo mode. Only sample config files will be used, such as sample_config.json and sample_config2.json")
+        # if os.path.exists(PATH_TO_CONFIG_FILES):
+        #     CONFIG_FILES = [FILE_NAME for FILE_NAME in os.listdir(PATH_TO_CONFIG_FILES) if FILE_NAME.startswith('sample_config') and FILE_NAME.endswith('.json')]
+        #     if CONFIG_FILES:
+        #         process_local_json(PATH_TO_CONFIG_FILES, CONFIG_FILES)
+        #     else:
+        #         process_online_json()
+        # else:
+        #     process_online_json()
+        process_online_json()
+    else:
+        if os.path.exists(PATH_TO_CONFIG_FILES):
+            CONFIG_FILES = [FILE_NAME for FILE_NAME in os.listdir(PATH_TO_CONFIG_FILES) if FILE_NAME.endswith('.json')]
+            process_local_json(PATH_TO_CONFIG_FILES, CONFIG_FILES)
+        else:
+            msg = 'PyDDNS cannot find or cannot access the ', PATH_TO_CONFIG_FILES, ' directory to access the configuration files.'
+            syslog.syslog(syslog.LOG_ERR, msg)
+            sys.exit(msg)
 
 #### Let's do it!
 make_it_so()
